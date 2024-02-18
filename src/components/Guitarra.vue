37189265e9c6d8473e13b625ccc6d038673ad3c7
Daniel Luengo <daniel.luengo.v@gmail.com>
2024-02-18 18:06:14 -0300

    test

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  guitarra: {
+  type:Object,
+  required: true
+}
+})
+</script>
+
+<template>
+
+<div   class="col-md-6 col-lg-4 my-4 row align-items-center">
+                <div class="col-4">
+                    <img class="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra">
+                </div>
+                <div class="col-8">
+                    <h3 class="text-black fs-4 fw-bold text-uppercase">{{guitarra.nombre}}</h3>
+                    <p>{{}}</p>
+                    <p class="fw-black text-primary fs-3">{{}}</p>
+                    <button 
+                        type="button"
+                        class="btn btn-dark w-100 "
+                    >Agregar al Carrito</button>
+                </div>
+            </div>
+
+</template>
+
+
+
+<style>
+
+
+</style>
